e28af912ed0e7222e581fb61a9d3f79a6ccda009
ajevans <ajecoder@gmail.com>
2020-11-25 16:23:34 -0500

    add msg update alert message

--- src/components/AutoDismissAlert/messages.js
+++ src/components/AutoDismissAlert/messages.js
@@ -5,5 +5,7 @@ export default {
   signInFailure: 'Failed to sign in. Check your email and password and try again.',
   signOutSuccess: 'Come back soon!',
   changePasswordSuccess: 'Password changed successfully!',
-  changePasswordFailure: 'Failed to change passwords. Check your old password and try again.'
+  changePasswordFailure: 'Failed to change passwords. Check your old password and try again.',
+  messageUpdateSuccess: 'Message updated successfully!',
+  messageUpdateFailure: 'Failed to update message, please try again.'
 }
