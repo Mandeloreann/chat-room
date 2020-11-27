96fe1854169ebfeb63e6d9513735c8f3211f46ae
ashivs2500 <antonioshivers2500@yahoo.com>
2020-11-27 16:27:14 -0500

    did some styling&routing edits
    11/27/2020
    added images
    changed some text styles
    altered some routes
    created some new routes (channels&chats)
    added a cool neon effect to title2

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -35,7 +35,7 @@ class SignUp extends Component {
         message: messages.signUpSuccess,
         variant: 'success'
       }))
-      .then(() => history.push('/'))
+      .then(() => history.push('/channels'))
       .catch(error => {
         this.setState({ email: '', password: '', passwordConfirmation: '' })
         msgAlert({
