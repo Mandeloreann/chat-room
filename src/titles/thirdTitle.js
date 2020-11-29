57d0d07bc74aa98b0c23ddb6e0dfcc9089fb4da8
ashivs2500 <antonioshivers2500@yahoo.com>
2020-11-29 17:31:13 -0500

    added some more styling
    added the chat box
    added the send arrow
    added borders to message box and send button

--- src/titles/thirdTitle.js
+++ src/titles/thirdTitle.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import './thirdTitle.scss'
 
 const chat = () => (
-  <div>
+  <Fragment>
     <p className="channels">CHANNELS</p>
-    <p className="chat">THE CHAT</p>
+    <p
+      className="chat">
+      THE CHAT
+      <button type="submit" className="sendMessage"></button>
+      <input type="text" name="chat[text]" placeholder="Type Your Message Here" className="typeMessage"></input>
+    </p>
     <p className="profile">3RD BOX FOR PROFILE</p>
-  </div>
+  </Fragment>
 )
 
 export default chat
