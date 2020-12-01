2347c853f50f20694556a209c09f6309b2eb8cb0
ttamsmas <ttamsmas@gmail.com>
2020-12-01 07:18:43 -0800

    Adding Chat Resource Axios Component to API Folder

--- src/api/chat.js
+++ src/api/chat.js
@@ -1,13 +1,13 @@
 import apiUrl from '../apiConfig'
 import axios from 'axios'
 
-export const createMessage = (chat, user) => {
+export const createMessage = user => {
   return axios({
     method: 'POST',
     url: apiUrl + '/chats',
     headers: {
       'Authorization': `Token token=${user.token}`
     },
-    data: { chat }
+    data: { chat: this.state.chat.text }
   })
 }
