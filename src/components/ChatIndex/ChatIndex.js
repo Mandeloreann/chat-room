496b483b94bc46a2b182959fee9a6a0f6ef50e91
ttamsmas <ttamsmas@gmail.com>
2020-12-01 07:03:08 -0800

    Correcting Merge Conflict before Pulling Dev Branch

--- src/components/ChatIndex/ChatIndex.js
+++ src/components/ChatIndex/ChatIndex.js
@@ -2,6 +2,9 @@ import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import '../../titles/thirdTitle.scss'
 
+import { createMessage } from '../../api/chat'
+import messages from '../AutoDismissAlert/messages'
+
 // import socket.io to establish socket connection with server
 import io from 'socket.io-client'
 
@@ -20,11 +23,20 @@ if (window.location.hostname === 'localhost') {
 class Chats extends Component {
   constructor (props) {
     super(props)
+    // console.log('this is ', this)
 
     this.state = {
-      chats: []
+      chats: [],
+      chat: {
+        text: ''
+      },
+      createdId: null,
+      owner: null
     }
   }
+  handleChange = event => this.setState({
+    [event.target.name]: event.target.value
+  })
 
   componentDidMount () {
     const socket = io(socketUrl, {
@@ -44,6 +56,40 @@ class Chats extends Component {
     //     chats: data
     //   })
     // })
+  }
+
+  handleInputChange = (event) => {
+    event.persist()
+    this.setState(prevState => {
+      const updatedField = {
+        [event.target.name]: event.target.value
+      }
+      const updatedData = Object.assign({}, prevState.chat, updatedField)
+      return { chat: updatedData }
+    })
+  }
+  onCreateMessage = (event) => {
+    event.preventDefault()
+
+    const { msgAlert } = this.props
+
+    createMessage(this.state)
+      .then(response => {
+        this.setState({ createdId: response.data.chat._id })
+      })
+      .then(() => msgAlert({
+        heading: 'Sent!',
+        message: messages.createMessageSuccess,
+        variant: 'success'
+      }))
+      .catch(error => {
+        this.setState({ text: '' })
+        msgAlert({
+          heading: 'Message failed ' + error.message,
+          message: messages.createMessageFailure,
+          variant: 'danger'
+        })
+      })
   }
 
   render () {
@@ -69,6 +115,19 @@ class Chats extends Component {
         <ul>
           {chats}
         </ul>
+
+        <div>
+          <h1>(username)</h1>
+          <form onSubmit={this.onCreateMessage}>
+            <input
+              placeholder="chat away..."
+              name="text"
+              value={this.state.chat.text}
+              onChange={this.handleInputChange}
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
         <button type="submit" className="sendMessageButton"></button>
         <textarea className="typeMessage" type="text" name="chat[text]" placeholder="Type Your Message Here"></textarea>
         <output type="text" name="chat[text]" className="sentMessage"></output>
