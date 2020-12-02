82e5326b2fcc800c294c8790a6441828a12327a0
ashivs2500 <antonioshivers2500@yahoo.com>
2020-12-02 11:06:58 -0500

    fixing

--- src/pages/thirdPage.js
+++ src/pages/thirdPage.js
@@ -1,28 +1,28 @@
-import React, { Fragment } from 'react'
+// import React, { Fragment } from 'react'
 
-import './thirdPage.scss'
+// import './thirdPage.scss'
 
-const ThirdPage = () => (
-  <Fragment>
-    <p
-      className="channels">
-      CHANNELS
-      <button type="button" className="channel1">English1</button>
-      <button type="button" className="channel2">English2</button>
-      <button type="button" className="channel3">Spanish1</button>
-      <button type="button" className="channel4">Spanish2</button>
-      <button type="button" className="channel5">Japanese1</button>
-      <button type="button" className="channel6">Japanese2</button>
-    </p>
-    <p
-      className="chat">
-      THE CHAT
-      <button type="submit" className="sendMessageButton"></button>
-      <textarea className="typeMessage" type="text" name="chat[text]" placeholder="Type Your Message Here"></textarea>
-      <output type="text" name="chat[text]" className="sentMessage"></output>
-    </p>
-    <p className="profile">MISC</p>
-  </Fragment>
-)
+// const ThirdPage = () => (
+//   <Fragment>
+//     <p
+//       className="channels">
+//       CHANNELS
+//       <button type="button" className="channel1">English1</button>
+//       <button type="button" className="channel2">English2</button>
+//       <button type="button" className="channel3">Spanish1</button>
+//       <button type="button" className="channel4">Spanish2</button>
+//       <button type="button" className="channel5">Japanese1</button>
+//       <button type="button" className="channel6">Japanese2</button>
+//     </p>
+//     <p
+//       className="chat">
+//       THE CHAT
+//       <button type="submit" className="sendMessageButton"></button>
+//       <textarea className="typeMessage" type="text" name="chat[text]" placeholder="Type Your Message Here"></textarea>
+//       <output type="text" name="chat[text]" className="sentMessage"></output>
+//     </p>
+//     <p className="profile">MISC</p>
+//   </Fragment>
+// )
 
-export default ThirdPage
+// export default ThirdPage
