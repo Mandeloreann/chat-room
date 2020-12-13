e125f0feb32b8183f47c1688db168e6658c46ead
ashivs2500 <antonioshivers2500@yahoo.com>
2020-12-12 23:30:32 -0500

    imported footer scss to footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,28 +1,11 @@
 import React from 'react'
 
-const style = {
-  backgroundColor: '#F8F8F8',
-  borderTop: '1px solid #E7E7E7',
-  textAlign: 'center',
-  padding: '20px',
-  position: 'absolute',
-  left: '0',
-  bottom: '0',
-  height: '60px',
-  width: '100%'
-}
-
-const phantom = {
-  display: 'block',
-  padding: '20px',
-  height: '60px',
-  width: '100%'
-}
+import './Footer.scss'
 
 const Footer = () => (
   <div>
-    <div style={phantom} />
-    <div style={style}>
+    <div className="phantom" />
+    <div className="copyRight">
       <p>© Copyright Mandeloreann 2020</p>
     </div>
   </div>
